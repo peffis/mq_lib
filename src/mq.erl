03bdd46bec3b283d2d0f2662c90a4322b63f85fe
Stefan Hellkvist <hellkvist@gmail.com>
2018-05-29 14:56:12 +0200

    adding support for nats

--- src/mq.erl
+++ src/mq.erl
@@ -12,7 +12,7 @@
 -export([terminate/2]).
 -export([code_change/3]).
 
--export([add_route/1, remove_route/1, cast/2, register_handler/1]).
+-export([add_route/1, add_route/2, remove_route/1, cast/2, register_handler/1]).
 
 
 -record(state, {
@@ -27,10 +27,13 @@
 start_link(Name) ->
     gen_server:start_link({local, Name}, ?MODULE, [], []).
 
-add_route(RoutingKey) when is_list(RoutingKey) ->
-    add_route(list_to_binary(RoutingKey));
 add_route(RoutingKey) ->
-    gen_server:cast(?MODULE, {add_route, RoutingKey}).
+    add_route(RoutingKey, #{}).
+
+add_route(RoutingKey, Opts) when is_list(RoutingKey) ->
+    add_route(list_to_binary(RoutingKey), Opts);
+add_route(RoutingKey, Opts) ->
+    gen_server:cast(?MODULE, {add_route, RoutingKey, Opts}).
 
 remove_route(RoutingKey) when is_list(RoutingKey) ->
     remove_route(list_to_binary(RoutingKey));
@@ -64,8 +67,8 @@ handle_call(_Request, _From, State) ->
     {reply, ignored, State}.
 
 
-handle_cast({add_route, Subject}, #state{connection = Conn} = State) ->
-    ok = nats:sub(Conn, Subject),
+handle_cast({add_route, Subject, Opts}, #state{connection = Conn} = State) ->
+    ok = nats:sub(Conn, Subject, Opts),
     {noreply, State};
 
 handle_cast({remove_route, Subject}, #state{connection = Conn} = State) ->
